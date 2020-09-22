dbc292a374757832095032c1cefd666b5b78671d
joochangyong <wnckddyd0525@gmail.com>
2020-09-22 17:13:41 +0900

    state학습

--- src/App.js
+++ src/App.js
@@ -4,40 +4,30 @@ import Content from "./components/Content";
 import Subject from "./components/Subject";
 import './App.css';
 
-//component학습
-/* class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>WEB</h1>
-        world wide web!
-      </header>
-    );
-  }
-}
-
-class Content extends Component{
-  render() {
-    return(
-      <article>
-        <h2>HTML</h2>
-        HTML is HyperText Markup Language.
-      </article>
-    );
-  }
-} */
-
 //유사 자바스크립트
 class App extends Component {
+  //state. 초기화를 담당
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'world wide web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is HyperText ...'},
+        {id:2, title:'CSS', desc:'CSS is for desing ...'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive ...'},
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        {/* component학습 */}
-        {/* <Subject></Subject>
-        <Content></Content> */}
-        {/* props학습 */}
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
+        {/* component 및 props학습 */}
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
