2c7af79a2639fdd3f3f464b424058d772b6698cc
joochangyong <wnckddyd0525@gmail.com>
2020-09-23 17:56:04 +0900

    이벤트 설치 및 mode 바꾸기(state 변경하기)

--- src/App.js
+++ src/App.js
@@ -10,25 +10,50 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      //무슨페이지인가?
+      mode:'read', 
       subject:{title:'WEB', sub:'world wide web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
-        {id:1, title:'HTML', desc:'HTML is HyperText ...'},
-        {id:2, title:'CSS', desc:'CSS is for desing ...'},
-        {id:3, title:'JavaScript', desc:'JavaScript is for interactive ...'},
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for desing'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
       ]
     }
   }
 
   render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+
+    }
     return (
       <div className="App">
         {/* component 및 props학습 */}
-        <Subject
+        {/* <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}>
-        </Subject>
+        </Subject> */}
+        <header>
+          <h1><a href="/" onClick={function(e){
+            console.log(e);
+            e.preventDefault();
+            //모드 바꾸기 bind까지
+            // this.state.mode = 'welcome';
+            this.setState({
+              mode:'welcome'
+            });
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
