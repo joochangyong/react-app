2fd202ca512c816fa30f01c916f41d2d965681aa
joochangyong <wnckddyd0525@gmail.com>
2020-10-06 16:57:26 +0900

    10.06

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
 import Subject from "./components/Subject";
+import Control from "./components/Control";
 import './App.css';
 
 //유사 자바스크립트
@@ -11,7 +13,7 @@ class App extends Component {
     super(props);
     this.state = {
       //무슨페이지인가?
-      mode:'read',
+      mode:'create',
       selected_content_id:1,
       subject:{title:'WEB', sub:'world wide web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
@@ -25,10 +27,11 @@ class App extends Component {
 
   render() {
     console.log('App render');
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if (this.state.mode === 'read'){
       var i = 0;
       while(i < this.state.contents.length){
@@ -40,8 +43,13 @@ class App extends Component {
         }
         i = i + 1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode === 'create') {
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        //setState를 통해서 새로운 contents값 추가 this.state.contents
+        console.log(_title, _desc);
+      }.bind(this)}></CreateContent>
     }
-
 
     return (
       <div className="App">
@@ -62,7 +70,12 @@ class App extends Component {
           }.bind(this)}
           data={this.state.contents}
         ></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          });
+        }.bind(this)}></Control>
+        {_article}
       </div>
     );
   }
